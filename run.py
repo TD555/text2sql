2f2f3e73c6fd48584deb5c3c9f59a8db301ffa78
TD555 <77773833+TD555@users.noreply.github.com>
2024-04-26 11:06:05 +0400

    Update run.py

--- run.py
+++ run.py
@@ -7,11 +7,22 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # model.do(device)
 tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
 
-# Example query
-query = "What is the average salary of employees in department 50?"
+# Example db_name, schema and query
+
+db_name = "product_catalog"
+
+schema = """| attribute_definitions : attribute_id , attribute_name , attribute_data_type | catalogs : catalog_id , catalog_name , catalog_publisher , date_of_publication , date_of_latest_revision | catalog_structure : catalog_level_number , 
+            catalog_id , catalog_level_name | catalog_contents : catalog_entry_id , catalog_level_number , parent_entry_id , previous_entry_id , next_entry_id , catalog_entry_name , product_stock_number , price_in_dollars , price_in_euros , price_in_pounds , capacity , 
+            length , height , width | catalog_contents_additional_attributes : catalog_entry_id , catalog_level_number , attribute_id , attribute_value | catalog_structure.catalog_id = catalogs.catalog_id | 
+            catalog_contents.catalog_level_number = catalog_structure.catalog_level_number | catalog_contents_additional_attributes.catalog_level_number = catalog_structure.catalog_level_number | catalog_contents_additional_attributes.catalog_entry_id = catalog_contents.catalog_entry_id |"""
+
+query = 'Which catalog contents have a product stock number that starts from "2"? Show the catalog entry names.'
+
+# Concatenate database name and schema with the query
+input_text = " ".join([db_name, schema, question])
 
 # Tokenize query
-input_ids = tokenizer.encode(query, return_tensors="pt").to(device)
+input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
 
 # Generate SQL query․ The max_length parameter is 20 by default, it should be changed for the number of generated tokens
 output_ids = model.generate(input_ids)
