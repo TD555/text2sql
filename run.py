fb591d622b4ca8aa1e2a065e6ec9898269ea8bc2
TD555 <77773833+TD555@users.noreply.github.com>
2024-04-26 12:05:21 +0400

    Update run.py

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ schema = """| attribute_definitions : attribute_id , attribute_name , attribute_
 query = 'Which catalog contents have a product stock number that starts from "2"? Show the catalog entry names.'
 
 # Concatenate database name and schema with the query
-input_text = " ".join([db_name, schema, question])
+input_text = " ".join([db_name, schema, query])
 
 # Tokenize query
 input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
